f93032fe44453a7ebfce863fc3aea6f16faee990
Aniketbhand05 <aniketbhand05@mail.com>
2022-12-21 10:15:40 +0530

    initial commit

---
+++ sub.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sub{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a = ");
+        int a = sc.nextInt();
+        System.out.print("enter b = ");
+        int b = sc.nextInt();
+        int c= a-b;
+        System.out.println("the sub is = "+ c);
+    }
+}
